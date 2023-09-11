f629f4f4c57bc52937277bbe4321dca927518ad5
Julio Guerra <j.guerratamayo@gmail.com>
2023-09-11 15:49:54 -0500

    Remove unused libraries imports

--- src/main/java/evershop/page_objects/CartPage.java
+++ src/main/java/evershop/page_objects/CartPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CartPage extends BaseComponent {
     By byCheckout = By.cssSelector("a[href='/checkout']");
